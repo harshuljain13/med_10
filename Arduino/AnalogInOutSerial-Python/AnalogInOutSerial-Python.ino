fc92c64d1b044f173240a3628e1aafc2c930edae
harshul1610 <harshulrobo@gmail.com>
2016-05-09 22:51:56 +0530

    first blood

---
+++ Arduino/AnalogInOutSerial-Python/AnalogInOutSerial-Python.ino
@@ -0,0 +1,15 @@
+
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+int sensorValue = 0;   
+
+void setup() {
+ 
+  Serial.begin(9600); 
+}
+
+void loop() {
+  
+  sensorValue = analogRead(analogInPin);
+  Serial.println(sensorValue);
+  delay(200);
+}
