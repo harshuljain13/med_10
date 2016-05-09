5a3fc6fefe7d664a742249102ec7ce7590d2a8ee
harshul1610 <harshulrobo@gmail.com>
2016-05-09 22:52:53 +0530

    frequency detection

---
+++ Arduino/frequency-test/frequency-test.ino
@@ -0,0 +1,59 @@
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+unsigned char rawData[80];
+const float sample_freq =34;
+
+//unsigned int i=0;
+int sensorValue = 0;        // value read from the pot
+int len = sizeof(rawData);
+int i,k;
+long sum, sum_old;
+int thresh = 0;
+float freq_per = 0;
+byte pd_state = 0;
+  
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+   // read the analog in value:
+  for(i=0;i<len;i++)
+  {
+  sensorValue = analogRead(analogInPin);
+  rawData[i]=sensorValue;
+  // print the results to the serial monitor:
+  //Serial.print("\nsensor = " );
+  //Serial.print(sensorValue);
+  delay(30);
+  }
+  
+  sum = 0;
+  pd_state = 0;
+  int period = 0;
+  // Autocorrelation
+  for(i=0; i < len; i++)
+  {
+    sum_old = sum;
+    sum = 0;
+    for(k=0; k < len-i; k++) sum += (rawData[k])*(rawData[k+i])/256;
+    
+    // Peak Detect State Machine
+    if (pd_state == 2 && (sum-sum_old) <=0) 
+    {
+      period = i;
+      pd_state = 3;
+    }
+    if (pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
+    if (!i) {
+      thresh = sum * 0.5;
+      pd_state = 1;
+    }
+  }
+  // Frequency identified in Hz
+  freq_per = sample_freq/period;
+  Serial.print("\nfreq=");
+  Serial.println(freq_per);
+  
+}
---
+++ Arduino/frequency_detect/frequency_detect.ino
@@ -0,0 +1,47 @@
+#include "C4.h"
+
+// Sample Frequency in Hz
+const float sample_freq = 22050;
+
+int len = sizeof(rawData);
+int i,k;
+long sum, sum_old;
+int thresh = 0;
+float freq_per = 0;
+byte pd_state = 0;
+  
+void setup() {
+  Serial.begin(9600);
+
+  sum = 0;
+  pd_state = 0;
+  int period = 0;
+  // Autocorrelation
+  for(i=0; i < len; i++)
+  {
+    sum_old = sum;
+    sum = 0;
+    for(k=0; k < len-i; k++) sum += (rawData[k]-128)*(rawData[k+i]-128)/256;
+    
+    // Peak Detect State Machine
+    if (pd_state == 2 && (sum-sum_old) <=0) 
+    {
+      period = i;
+      pd_state = 3;
+    }
+    if (pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
+    if (!i) {
+      thresh = sum * 0.5;
+      pd_state = 1;
+    }
+  }
+  // Frequency identified in Hz
+  freq_per = sample_freq/period;
+  Serial.println(freq_per);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
+
